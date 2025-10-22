343f1ad4a3b29a1a826e4e97cbe5cef1807b7962
João Vitor <olhierturcojoaovitor@gmail.com>
2025-10-22 16:06:34 -0300

    transformando o codigo em orientação a objetos

--- script.js
+++ script.js
@@ -1,5 +1,65 @@
 import { Modulopalavras } from './palavra/Modulopalavra.js' // importa o modulo da lista de palavras
 
+class jogo{
+    constructor(){
+        this.palavraOculta = Modulopalavras[Math.floor(Math.random() * Modulopalavras.length)]
+        this.posicaoAtual = 0
+        this.linhaAtual = 0
+        this.palavraJogada = ''
+        this.linhas = document.querySelectorAll('.linha')
+    }
+
+    //Metodo que pega sempre a linha atual em que esta o jogador
+    getLetrasAtuais(){
+        return this.linhas[this.linhaAtual].querySelectorAll('.letter')
+    }
+
+    //Metodo de adicionar letra as celulas
+    addLetra(tecla){
+        const letras = this.getLetrasAtuais()
+
+        if(/^[a-z]$/i.test(tecla) && this.posicaoAtual < letras.length){
+            letras[this.posicaoAtual].textContent = tecla.toUpperCase()
+            this.posicaoAtual++
+        }
+    }
+
+    //Metodo de apagar as letras das celulas
+    deleteLetras(){
+        const letras = this.getLetrasAtuais()
+
+        if(this.posicaoAtual > 0){
+            this.posicaoAtual--
+            letras[this.posicaoAtual].textContent = ''
+        }
+    }
+}
+
+const jogo1 = new jogo()
+
+document.addEventListener('keydown', (event) => {
+    const tecla = event.key.toLowerCase()
+
+    if(event.key === 'Backspace'){
+        jogo1.deleteLetras()
+    }else{
+        jogo1.addLetra(tecla)
+    }
+})
+
+
+
+
+
+/*
+//funcao que adiciona as letras
+function addLetras(tecla){
+    const letras = getLetrasAtuais()
+    if(/^[a-z]$/i.test(tecla) && posicaoAtual < letras.length){
+        letras[posicaoAtual].textContent = tecla.toUpperCase()
+        posicaoAtual++
+    }
+}
 let posicaoAtual = 0
 let linhaAtual = 0
 const linhas = document.querySelectorAll('.linha')
@@ -65,7 +125,7 @@ document.addEventListener('keydown', (event) => {
             event.preventDefault()
             capturarLetras()
             let palavraJogada = palavradoJogador
-            if(palavraJogada.includes(Modulopalavras)){
+            if(Modulopalavras.includes(palavraJogada)){
                 console.log('tem')
             }else{
                 console.log('Nao tem')
@@ -76,5 +136,5 @@ document.addEventListener('keydown', (event) => {
             addLetras(tecla)
             
     }
- })
-
+})
+*/
